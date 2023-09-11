b52217c0afb669b47b1dd474b96e4574e297e620
Kunga Derick Abongho <124506420+Dericko681@users.noreply.github.com>
2023-09-11 11:55:22 +0100

    contains the code for the first page of this website.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML>
+    <body>
+        <?php
+echo "<link rel='stylesheet' href='indexstyle.css'>
+";
+echo "<P> Welcome to my Todo list application </P>";
+echo "<p> This is a task  management tool that allows
+you to create, organise and track your tasks.</p>";
+
+echo "<p> you may signup to create an acount</p>";
+echo "<button type='submit' name='submitsignup'> signup </button>";
+
+echo "<p> or may login to an existing account</p>";
+echo "<button  type='submit' name='submitlogin'> login </button>";
+
+
+
+
+?>
+</body>
+</html>
